67dcda24003daa3787beb718fb73af3f144b599e
01xalphaai <01x.alpha.ai@gmail.com>
2025-07-29 03:18:49 +0000

    Basic

---
+++ 01_Basic/basic.py
@@ -0,0 +1,20 @@
+a=4
+b = 5 
+v = 7
+f = a+b+v
+# print(f)
+s =33
+p = 44
+k = 99
+l = 66
+o = 11 
+y= s/o
+i = p + o
+w = l-o 
+e = o*k
+arpit = "this is your first class. "
+shukla = "this is my surname. "
+shivbhaiya = "my teachers is very "
+quality = "kind and humble "
+print(a, b,v,f,y,i,w,e,arpit, shukla, shivbhaiya, quality)
+print (type (shukla))
