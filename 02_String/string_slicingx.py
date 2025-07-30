aa69dc7919fd8fffe322016bbfd5d002557839a7
01xalphaai <01x.alpha.ai@gmail.com>
2025-07-30 04:34:09 +0000

    string

---
+++ 02_String/string_slicingx.py
@@ -0,0 +1,19 @@
+# Indexing
+Arpit = "Name"
+print(Arpit[0])
+print(Arpit[1])
+print(Arpit[2])
+print(Arpit[3])
+
+# White space - space which we give that counts in code
+
+# string slicing in positive
+slice = "Arpit, Shiv Singh"
+
+print(slice[2:]) # 0:(n-1)
+print(slice[:5]) # 0:(n-1)
+print(slice[0:5]) # 0:(n-1)
+
+# string slicing in Negative
+negslice = "Apple"
+
---
+++ 02_String/stringx.py
@@ -0,0 +1,23 @@
+
+#Ways of defing string
+
+a = "this is a way of defing a string"
+
+b = 'this is "too good" to eat'
+
+c = '''Lorem Ipsum is "simply" dummy 'text' of the printing 
+and typesetting industry. print(a)Lorem Ipsum has be unchanged.'''
+
+print(c)
+
+# uppercase, lowercase, capatilize
+
+uppercase = "THIS IS UPPERCASE TEXT"
+lowercase = "this is lowercase text"
+capatilize = "This Is Capatilize Text"
+print(uppercase, lowercase, capatilize)
+
+# parenthis:- ()
+# bracket:- []
+# braces:- {}
+
