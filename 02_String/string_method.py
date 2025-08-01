d62e7c111588018eff0d21b82a5b3268337e5e0a
01xalphaai <01x.alpha.ai@gmail.com>
2025-08-01 03:43:52 +0000

    string method

---
+++ 02_String/string_method.py
@@ -0,0 +1,102 @@
+# start practice for sting method
+
+
+# 01 uppercase method
+# a = "this is example of upper case"
+# b = "this is second example "
+# c = "abcde"
+# d = "mncbi"
+# print(a.upper())
+# print(b.upper())
+# print(c.upper())
+# print(d.upper())
+
+# 02 lower case method
+# e = "ABCDEF"
+# F = "ARPIT"
+# G = "EXCEL"
+# print(e.lower())
+# print(F.lower())
+# print(G.lower())
+
+# 03 rstrip method
+# h = "just do it !@#$!"
+# i = "iamadequate %^&"
+# j = "work hard +-&*"
+# print(h.rstrip("!@#$!"))
+# print(i.rstrip("%^&"))
+# print(j.rstrip("+-&*"))
+
+# 04 replace method 
+# k = "you can"
+# l = "only one is sufficient"
+# m = "you can understand "
+# print(k.replace("can","can't"))
+# print(l.replace("one","two"))
+# print(m.replace("understand","do it"))
+
+# 05 spillting method
+# n = "one two three foue five six"
+# o = "arpit ram ganesh vishnu abhimanu"
+# p = "monday tuesday thusday wednesday friday "
+# print(n.split(" "))
+# print(o.split(" "))
+# print(p.split(" "))
+ 
+# 06 capatilize method
+# q = "education is must for everyone"
+# r = "i am not ai"
+# s = "human being"
+# print(q.capitalize())
+# print(r.capitalize())
+# print(s.capitalize())
+
+# 07 string center method
+# t = " have a nice day. "
+# u = " i do my best ."
+# print(t.center(30))
+# print(u.center(30))
+
+# 08 count method 
+# v = " abcdef abcdef abcdef"
+# w = " this is the method of sting method"
+# x = " monday kab hai monday kal hai"
+# print(v.count("abcdef"))
+# print(w.count("method"))
+# print(x.count("hai"))
+
+# 09 endswith metod
+# y = " i like play chess."
+# z = "i like play football"
+# print(y.endswith("."))
+# print(z.endswith("."))
+
+# 10 starwith method 
+# aa = "chess is my favourate game."
+# bb = "football is my favourate game."
+# print(aa.startswith("chess"))
+# print(bb.startswith("football"))
+
+
+# final practice of all the types of string method 
+a = "parshav infotech."
+b = "PARSHAV INFOTECH."
+c = "prashav infotech !@."
+d = "prashav infotech."
+e = "prashav infotech."
+f = "prashav  prashav infotech."
+g = "prashav infotech."
+h = "prashav infotech."
+i = "prashav infotech."
+j = "prashav infotech."
+print(a.upper())
+print(b.lower())
+print(c.rstrip("!@"))
+print(d.capitalize())
+print(e.split(" "))
+print(f.count("prashav"))
+print(g.endswith("."))
+print(h.startswith("."))
+print(i.replace("prashav","mahaveer"))
+
+
